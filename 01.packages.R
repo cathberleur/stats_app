44d373542db316b24fd7daf68689e12d7ff0b711
cbracq1 <clemence.bracq@ens-lyon.fr>
2023-02-08 11:37:04 +0100

    Create 01.packages.R

---
+++ 01.packages.R
@@ -0,0 +1,27 @@
+## Installation
+list_packages <- c("tidyverse", "readxl", "ggplot2", "data.table", "dplyr", "stargazer", "cartography","sf","rgdal","readr", "sp", "stringr", "questionr", "FactoMineR", "factoextra", "Factoshiny", "explor")
+new_packages <- list_packages[!(list_packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
+
+## Chargement
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(stargazer)
+library(cartography)
+library(sf)
+library(rgdal)
+library(readr)
+library(sp)
+library(stringr)
+library(questionr)
+library(FactoMineR)
+library(factoextra)
+library(Factoshiny)
+library(explor)
+
+
+
